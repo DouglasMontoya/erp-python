9576db1f806111aa94f1b23aaf7515463274d7ed
Ruben Vasquez <ruvasqm@gmail.com>
2023-11-10 15:46:54 +0000

    Fix local dev environment

---
+++ app/db/session.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import create_database, database_exists
+
+from app.core import config
+
+engine = create_engine(
+    config.SQLALCHEMY_DATABASE_URI,
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def validate_database():
+    if not database_exists(engine.url):
+        create_database(engine.url)
