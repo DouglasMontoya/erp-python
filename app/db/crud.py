22b1e9c2f9b2a2154bea5b76607e09760b122cfb
DouglasMontoya <douglasmontoyalbjf@gmail.com>
2024-02-14 22:26:33 -0400

    Refactor: Refactor the code for adding addresses, adding payment methods and adding contacts

--- app/db/crud.py
+++ app/db/crud.py
@@ -72,6 +72,7 @@ def account_create(db: Session, data):
     if len(data['step1']['clientType']) == 1:
         customer_type = data['step1']['clientType'][0]
         if customer_type == 'business-client':
+          
             # CREATE CUSTOMER
             tradename = data['step1']['commercialName']
             email_db = data['step1']["email"]
@@ -83,9 +84,6 @@ def account_create(db: Session, data):
                 tradename=tradename,
                 is_customer=customer_type,
                 legal_representative=legal_repres
-
-
-                # Add any other fields as necessary
             )
 
             # Add the new customer to the session and commit
@@ -95,107 +93,175 @@ def account_create(db: Session, data):
             # Get customer id
             customer_id = customer.id
 
-            # ADD ADDRESSES
-
-            # Main address
-            address_1 = {
-                "customer_id": customer_id,
-                "company": data['step1']['fiscalName'],
-                "nif": data['step1']['nif'],
-                "address_1": data['step1']['address'],
-                "postal_code": data['step1']['postalCode'],
-                "province": data['step1']['province'],
-                "city": data['step1']['city'],
-                "first_name": data['step1']['peopleContact'],
-                "phone": data['step1']['phone'],
-                "company_activity": data['step1']['companyActivity'],
-                "cnae": data['step1']['cnae'],
-                "opening_days": data['step1']['daysOpened'],
-                "horary": data['step1']['hours'],
-                "tags": data['step1']['tags'],
-            }
-
-            # Optional address
-            new_address = models.Address(
-                customer_id=address_1["customer_id"],
-                company=address_1["company"],
-                nif=address_1["nif"],
-                address_1=address_1["address_1"],
-                postal_code=address_1["postal_code"],
-                province=address_1["province"],
-                city=address_1["city"],
-                first_name=address_1["first_name"],
-                phone=address_1["phone"],
-                company_activity=address_1["company_activity"],
-                cnae=address_1["cnae"],
-                opening_days=address_1["opening_days"],
-                horary=address_1["horary"],
-                tags=address_1["tags"],
-            )
-
-            db.add(new_address)
-            db.commit()
-
-            for item in data['step2']:
-                new_address_data = {
-                    "customer_id": customer_id,
-                    "company": item['fiscalName'],
-                    "nif": item['nif'],
-                    "address_2": item['address'],
-                    "postal_code": item['postalCode'],
-                    "province": item['province'],
-                    "city": item['city'],
-                    "first_name": item['peopleContact'],
-                    "phone": item['phone'],
-                    "company_activity": data['step1']['companyActivity'],
-                    "cnae": data['step1']['cnae'],
-                    "opening_days": item['daysOpened'],
-                    "horary": item['hours'],
-                }
-                new_address = models.Address(**new_address_data)
-                db.add(new_address)
-                db.commit()
+            # ADD ADDRESS
+            add_addresses(db, customer_id, data)
 
             # ADD PAYMENT METHOD
-            payment_method_object = {}
-            if data['step3']['payMethod'] == 'bank':
-                payment_method_object = {
-                    "customer_id": customer_id,
-                    "iban": data['step3']['iban'],
-                    "swift": data['step3']['bicSwift'],
-                    "currency_code": data['step3']['currency'],
-                    "bank_country": data['step3']['countryBankId'],
-                    "payment_method": data['step3']['payMethod']
-                }
-            else:
-                pass
-
-            payment_method = models.CustomerPayment(**payment_method_object)
-
-            db.add(payment_method)
-            db.commit()
+            add_method_payment(db, customer_id, data)
 
             # ADD CONTACTS
-            for item in data['step4']:
-                object_contact = {
-                    "customer_id": customer_id,
-                    "note": item['comments'],
-                    "email_notification": item['emailNotifications'],
-                    "email": item['email'],
-                    "phone": item['phone'],
-                    "city": item['city'],
-                    "address": item['address'],
-                    "job": item['job'],
-                    "dni": item['dni'],
-                    "name": item['nameAndLastName'],
-                }
-                contacts = models.Contacts(**object_contact)
-                db.add(contacts)
-                db.commit()
+            add_contacts(db, customer_id, data)
 
             return {"message": "Usuario creado exitosamente", "data": "ok"}
             # return {"message": "Usuario creado exitosamente", "data": customer}
+        elif customer_type == 'provider':
+
+            # CREATE CUSTOMER
+
+            object_customer = {
+                "tradename": data['step1']['commercialName'],
+                "email": data['step1']["email"],
+                "legal_representative": data['step1']["represent"],
+                "is_customer": customer_type
+            }
+
+            new_customer = models.Customer(**object_customer)
+            db.add(new_customer)
+            db.commit()
+
+            # Get customer id
+            customer_id = new_customer.id
+
+            # CREATE PROVIDER
+
+            object_provider = {
+                "customer_id": customer_id,
+                "operation_type_id": data['step1']['operationType'],
+                "operation_field_id": data['step1']['operationScope'],
+            }
+
+            new_provider = models.Provider(**object_provider)
+            db.add(new_provider)
+            db.commit()
+
+            # ADD ADDRESS
+            add_addresses(db, customer_id, data)
+
+            # ADD PAYMENT METHOD
+            add_method_payment(db, customer_id, data)
+
+            # ADD CONTACTS
+            add_contacts(db, customer_id, data)
+
+        elif customer_type == 'particular-client':
+            
+            # CREATE CUSTOMER
+            object_customer = {
+                "fisrt_name": data['step1']['firstName'],
+                "last_name": data['spet1']['lastName'],
+                "email": data['spet1']['email'],
+            }
+
+            # Create a new Customer instance
+            customer = models.Customer(**object_customer)
+
+            # Add the new customer to the session and commit
+            db.add(customer)
+            db.commit()
+
+            # Get customer id
+            customer_id = new_customer.id
+
+            # ADD ADDRESS
+
+            add_addresses(db, customer_id, data)
+
         else:
             raise HTTPException(status_code=400, detail="Invalid client type")
     else:
         raise HTTPException(status_code=400, detail="Invalid data")
+    
+    return {"message": "Cuena creada exitosamente", "data": "ok"}
+
+
+def add_addresses(db: Session, customer_id, data):
+
+    # ADD ADDRESSES
+
+    # Main address
+    address_1 = {
+        "customer_id": customer_id,
+        "company": data['step1']['fiscalName'],
+        "nif": data['step1']['nif'],
+        "address_1": data['step1']['address'],
+        "postal_code": data['step1']['postalCode'],
+        "province": data['step1']['province'],
+        "city": data['step1']['city'],
+        "first_name": data['step1']['peopleContact'],
+        "phone": data['step1']['phone'],
+        "company_activity": data['step1']['companyActivity'],
+        "opening_days": data['step1']['daysOpened'],
+        "horary": data['step1']['hours'],
+        "tags": data['step1']['tags'],
+    }
+    
+    if data['step1']['clientType'] == 'business-client':
+        address_1['cnae'] = data['step1']['cnae']
+
+    # Optional address
+    new_address = models.Address(**address_1)
+
+    db.add(new_address)
+    db.commit()
+
+    for item in data['step2']:
+        new_address_data = {
+            "customer_id": customer_id,
+            "company": item['fiscalName'],
+            "nif": item['nif'],
+            "address_2": item['address'],
+            "postal_code": item['postalCode'],
+            "province": item['province'],
+            "city": item['city'],
+            "first_name": item['peopleContact'],
+            "phone": item['phone'],
+            "company_activity": data['step1']['companyActivity'],
+            "opening_days": item['daysOpened'],
+            "horary": item['hours'],
+        }
+        if data['step1']['clientType'] == 'business-client':
+            new_address_data['cnae'] = data['step1']['cnae']
+
+        new_address = models.Address(**new_address_data)
+        db.add(new_address)
+        db.commit()
+
+
+def add_method_payment(db: Session, customer_id, data):
+    payment_method_object = {}
+    if data['step3']['payMethod'] == 'bank':
+        payment_method_object = {
+            "customer_id": customer_id,
+            "iban": data['step3']['iban'],
+            "swift": data['step3']['bicSwift'],
+            "currency_code": data['step3']['currency'],
+            "bank_country": data['step3']['countryBankId'],
+            "payment_method": data['step3']['payMethod']
+        }
+    else:
+
+        # Add when the method payment is transfer
+        pass
+
+    payment_method = models.CustomerPayment(**payment_method_object)
+    db.add(payment_method)
+    db.commit()
+
+
+def add_contacts(db: Session, customer_id, data):
+    for item in data['step4']:
+        object_contact = {
+            "customer_id": customer_id,
+            "note": item['comments'],
+            "email_notification": item['emailNotifications'],
+            "email": item['email'],
+            "phone": item['phone'],
+            "city": item['city'],
+            "address": item['address'],
+            "job": item['job'],
+            "dni": item['dni'],
+            "name": item['nameAndLastName'],
+        }
+        contacts = models.Contacts(**object_contact)
+        db.add(contacts)
+        db.commit()
